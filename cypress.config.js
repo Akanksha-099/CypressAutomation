54cf67dec6f834ecc7cec349c6d55318ffd0ed9f
Akanksha-099 <akankshabhasin36890@gmail.com>
2023-05-01 15:34:02 +0530

    Refactored dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -10,11 +10,14 @@ module.exports = defineConfig({
     responseTimeout:30000,
     screenshotOnRunFailure:true,
     video:true,
+    videosFolder:cypress/videos,
+    videoCompression: 15,
     reporterOptions: {
     charts: true,
     reportPageTitle: 'Cypress Inline Reporter',
     embeddedScreenshots: true,
     inlineAssets: true, //Adds the asserts inline
+    
 },
  
   e2e: {
