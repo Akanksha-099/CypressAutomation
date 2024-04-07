493420788c0121892663dd75973e4055ca0debcf
Akanksha-099 <80638734+Akanksha-099@users.noreply.github.com>
2024-04-07 18:21:13 +0530

    Added analytics test

--- cypress/e2e/TC07_AdobeAnalyticsTesting.cy.js
+++ cypress/e2e/TC07_AdobeAnalyticsTesting.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="Cypress" />
+
 import '../../cypress/support/commands.js';
 describe('Testing Adobe Analytics', () => {
-    it('Validate Adobe Analytics variables', () => {
+    it.skip('Validate Adobe Analytics variables', { retries: 2 },() => {
         // Visit the URL of the article
         cy.visit('https://www.mckinsey.com/careers/the-city-cup/faqs')
         cy.handleCookieBanner() 
         // Intercept the Adobe Analytics request
         cy.intercept('/b/ss/**').as('adobe')
-      
+        cy.scrollTo('60%',5000)
         // Click on the accordion item to expand
         cy.get('button[data-layer-location="accordion"]').first().click({force:true})
       
@@ -45,7 +46,7 @@ describe('Testing Adobe Analytics', () => {
         })
       })
 
-    it('Validate Adobe Analytics variables', () => {
+    it.skip('Validate Adobe Analytics variables', { retries: 2 }, () => {
         // Visit the URL of the article
         cy.visit('https://www.mckinsey.com/capabilities/risk-and-resilience/our-insights/implementing-generative-ai-with-speed-and-safety')
         cy.handleCookieBanner() 
@@ -89,18 +90,51 @@ describe('Testing Adobe Analytics', () => {
           })
         })
       })
-    it.only('Validate Adobe analtics variables', () => {
+    it('Validate Adobe analytics variables', () => {
         cy.intercept('/b/ss/**').as('adobe')
         cy.visit('https://promos.marriott.com/en/GROUP/golden-triangle')
-        
-        
         cy.scrollTo('bottom')
-       
-       
         cy.wait('@adobe').then(interception => {
             // Extract the values from the intercepted request
             const v41ValueExpand = new URL(interception.request.url).searchParams.get('v41')
             expect(v41ValueExpand).to.eq('Augustine, a Luxury Collection Hotel, Prague')
         })
+    })
+    it('Validate Adobe analytics variables',()=>{
+      cy.intercept('/b/ss/**').as('adobe')
+      cy.visit('https://www.marriott.com/en-us/hotels/laspm-park-mgm-las-vegas-a-tribute-portfolio-resort/rooms/')
+      cy.scrollTo('bottom')
+      cy.get('.onCorePage > .faq-accordion-faq-question').click()
+      cy.wait('@adobe').then(interception => {
+        // Extract the values from the intercepted request
+        const c26ValueExpand = new URL(interception.request.url).searchParams.get('c26')
+        const v22ValueExpand = new URL(interception.request.url).searchParams.get('v22')
+        const v192ValueExpand = new URL(interception.request.url).searchParams.get('v192')
+        const linkNameValueExpand = new URL(interception.request.url).searchParams.get('pev2')
+        const eventsValueExpand = new URL(interception.request.url).searchParams.get('events')
+
+        expect(c26ValueExpand).to.eq('AEM-prod') // Adjust the expected value based on your scenario
+        expect(v22ValueExpand).to.eq('unauthenticated')
+        expect(v192ValueExpand).to.eq('www.marriott.com/en-us/hotels/laspm-park-mgm-las-vegas-a-tribute-portfolio-resort/rooms/')
+        expect(eventsValueExpand).to.eq('event1,prodView,event441')
+        expect(linkNameValueExpand).to.eq(null)
+
+        cy.get('.onCorePage > .faq-accordion-faq-question').click()
+        cy.wait('@adobe').then(collapseInterception => {
+          // Extract the values from the intercepted request after collapsing
+          const c26ValueCollapse = new URL(collapseInterception.request.url).searchParams.get('c26')
+          const v22ValueCollapse = new URL(collapseInterception.request.url).searchParams.get('v22')
+          const v192ValueCollapse = new URL(interception.request.url).searchParams.get('v192')
+          const eventsValueCollapse = new URL(collapseInterception.request.url).searchParams.get('events')
+          const linkNameValueCollapse = new URL(collapseInterception.request.url).searchParams.get('pev2')
+    
+          // Perform assertions for the collapsed state
+          expect(c26ValueCollapse).to.eq('AEM-prod') // Adjust the expected value based on your scenario
+          expect(v22ValueCollapse).to.eq('unauthenticated')
+          expect(v192ValueCollapse).to.eq('www.marriott.com/en-us/hotels/laspm-park-mgm-las-vegas-a-tribute-portfolio-resort/rooms/')
+          expect(eventsValueCollapse).to.eq('event2')
+          expect(linkNameValueCollapse).to.eq('Custom Link Click')
+        })
+      })
     })
 })
